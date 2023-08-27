bc8040fae50d7682fdd5bb691d27528a156315ca
Armand Robinson <UnitedGaming44@gmail.com>
2023-08-27 14:09:42 -0400

    make it better

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 require("dotenv").config();
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const hotelsRoute = require("./routes/hotels");
-
 const path = require("path");
-const cookieParser = require("cookie-parser");
 
 const app = express();
 
 //middlewares
 app.use(cors());
-app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/hotels", hotelsRoute);
