a1754c0701256249b3d828faafe34e355261e28b
Armand Robinson <UnitedGaming44@gmail.com>
2023-08-27 10:07:53 -0400

    make it better

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
+require("dotenv").config();
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
 const app = express();
-dotenv.config();
 
 mongoose.connection.on("disconnected", () => {
   console.log("mongoDB disconnected!");
