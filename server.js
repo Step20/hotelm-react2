232bda4b051826f5ce9a1e381f686c94d7119e11
Armand Robinson <UnitedGaming44@gmail.com>
2023-08-27 14:36:10 -0400

    make it better

--- server.js
+++ server.js
@@ -24,6 +24,21 @@ mongoose
     console.log(err);
   });
 
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
