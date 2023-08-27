4e40b5eaeb968eed8148d5fb58331c400f5b25a2
Armand Robinson <UnitedGaming44@gmail.com>
2023-08-27 15:38:39 -0400

    make it better

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ const app = express();
 
 //middlewares
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/hotels", hotelsRoute);
 
-mongoose.set("strictQuery", false);
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -23,21 +23,6 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-mongoose.connection.on("disconnected", () => {
-  console.log("mongoDB disconnected!");
-});
-
-app.use((err, req, res, next) => {
-  const errorStatus = err.status || 500;
-  const errorMessage = err.message || "Something went wrong!";
-  return res.status(errorStatus).json({
-    success: false,
-    status: errorStatus,
-    message: errorMessage,
-    stack: err.stack,
-  });
-});
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
