99e9daf73bebe20d5a8b23981b67f4e51505c4ad
Armand Robinson <UnitedGaming44@gmail.com>
2023-08-27 11:05:23 -0400

    make it better

--- controllers/room.js
+++ controllers/room.js
@@ -1,5 +1,5 @@
 import Room from "../models/Room.js";
-import Hotel from "../models/hotel.js";
+import Hotel from "../models/Hotel.js";
 import { createError } from "../utils/error.js";
 
 export const createRoom = async (req, res, next) => {
