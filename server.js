343b5f6b6d8ec8b112f56a556c3f9a8abafe86e8
Armand Robinson <UnitedGaming44@gmail.com>
2023-08-27 15:41:34 -0400

    make it better

--- server.js
+++ server.js
@@ -15,24 +15,39 @@ app.use(express.json());
 
 app.use("/api/hotels", hotelsRoute);
 
-mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log(err);
+mongoose.set("strictQuery", false);
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
   });
+});
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*", function (req, res) {
-    const index = path.join(__dirname, "build", "index.html");
-    res.sendFile(index);
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
-const PORT = process.env.PORT || 8800;
-app.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to backend.");
 });
