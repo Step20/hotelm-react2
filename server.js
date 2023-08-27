b4e0a6f7f8ac3312d718ebfcc755f8b5af8bb97a
Armand Robinson <UnitedGaming44@gmail.com>
2023-08-27 16:12:36 -0400

    make it better

--- server.js
+++ server.js
@@ -24,11 +24,13 @@ const connect = async () => {
     throw error;
   }
 };
-app.use(express.static("client/build"));
-// send the user to index html page inspite of the url
-app.get("*", (req, res) => {
-  res.sendFile(path.join("client", "index.html"));
-});
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 8800;
 app.listen(PORT, () => {
