38bc75aff773e39b306caa876a5e7cbe3aa5777f
Armand Robinson <UnitedGaming44@gmail.com>
2023-08-27 17:55:02 -0400

    make it better

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ mongoose
   });
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname, "/client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+  app.use("/static", express.static(path.join(`${__dirname}/client/build`)));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/`));
   });
 }
 
