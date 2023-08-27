5150730b6ba5baeef2c76025b1a3c1e65171961c
Armand Robinson <UnitedGaming44@gmail.com>
2023-08-27 17:57:32 -0400

    make it better

--- server.js
+++ server.js
@@ -13,12 +13,6 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/static", express.static(path.join(`${__dirname}/client/build`)));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(`${__dirname}/client/build/`));
-  });
-}
 app.use("/api/hotels", hotelsRoute);
 
 mongoose
@@ -29,6 +23,16 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 8800;
 app.listen(PORT, () => {
