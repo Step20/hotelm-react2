8f469848377b567c7f5db0b5727994a7029e8fa9
Armand Robinson <UnitedGaming44@gmail.com>
2023-08-27 16:08:11 -0400

    make it better

--- server.js
+++ server.js
@@ -25,25 +25,10 @@ const connect = async () => {
   }
 };
 
-mongoose.connection.on("disconnected", () => {
-  console.log("mongoDB disconnected!");
-});
-
-app.use((err, req, res, next) => {
-  const errorStatus = err.status || 500;
-  const errorMessage = err.message || "Something went wrong!";
-  return res.status(errorStatus).json({
-    success: false,
-    status: errorStatus,
-    message: errorMessage,
-    stack: err.stack,
-  });
-});
-
 app.use(express.static(__dirname));
 // send the user to index html page inspite of the url
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "index.html"));
 });
 
 const PORT = process.env.PORT || 8800;
