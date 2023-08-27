b49b91b33a182acd5e4848925ba51fa50e330992
Armand Robinson <UnitedGaming44@gmail.com>
2023-08-27 13:07:07 -0400

    make it better

--- index.js
+++ index.js
@@ -50,5 +50,5 @@ if (process.env.NODE_ENV === "production") {
 
 app.listen(process.env.PORT || 8800, () => {
   connect();
-  console.log("Server is running on port", PORT);
+  console.log("Server is running on port", process.env.PORT);
 });
