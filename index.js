e00d00aab117df5bbb8718873d59e73555a6a72c
Armand Robinson <UnitedGaming44@gmail.com>
2023-08-27 13:16:42 -0400

    make it better

--- index.js
+++ index.js
@@ -42,13 +42,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
-
 const PORT = process.env.PORT || 8800;
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
