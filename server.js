2d7c1d2c1da7c2970e6ff408ce82883912c3e456
Armand Robinson <UnitedGaming44@gmail.com>
2023-08-28 08:26:43 -0400

    make it better

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ mongoose
 if (process.env.NODE_ENV === "production") {
   app.use(
     "/static",
-    express.static(path.join(__dirname, "client", "build/static"))
+    express.static(path.join(__dirname, "client/build/static"))
   );
   app.use(
     "/manifest.json",
-    express.static(path.join(__dirname, "client", "build", "manifest.json"))
+    express.static(path.join(__dirname, "client/build/manifest.json"))
   );
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
