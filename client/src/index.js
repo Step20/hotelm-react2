0f6a7900ea230747d6139d1076a42ac226d7b4fb
Armand Robinson <UnitedGaming44@gmail.com>
2023-08-27 22:20:16 -0400

    make it better

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorker from "./serviceWorker";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
